7638dd4f7a843ff030234bdef3001e138db7c11c
pamabe <18455232857@163.com>
2020-04-14 12:42:49 +0800

    composer hello

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+   * Created by PhpStorm.
+    * User: shiwenyuan
+	 * Date: 2018/8/2 13341007105@163.com
+	  * Time: 下午8:56
+	   */
+require_once __DIR__."/vendor/autoload.php";
+//require_once __DIR__."/src/SayHello.php";
+
+use Hello\SayHello;
+SayHello::world();
+
